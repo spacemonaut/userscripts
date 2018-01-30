4af1bedc1384f3c7af3fd59876d89f20068fcd7f
spacemonaut <4585677+spacemonaut@users.noreply.github.com>
2018-01-30 14:36:42 +0000

    add scryfall tagger script

---
+++ scryfall-tagger-enhancements/README.md
@@ -0,0 +1,12 @@
+# Scryfall Tagger Enhancements
+
+This script is a small project to make some enhancements and updates to the Scryfall tagger interface.
+
+Right now it does one specific thing: Pagination.
+
+![prev/next buttons below the card image](preview.png)
+
+# [Click here to install](https://raw.githubusercontent.com/spacemonaut/userscripts/master/scryfall-tagger-enhancements/scryfall-tagger-enhancements.user.js)
+
+* Firefox users will need [Greasemonkey](https://addons.mozilla.org/en-GB/firefox/addon/greasemonkey/) or [Tampermonkey](https://addons.mozilla.org/en-GB/firefox/addon/tampermonkey/)
+* Chrome users will need [Tampermonkey](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo?hl=en)
---
+++ scryfall-tagger-enhancements/scryfall-tagger-enhancements.user.js
@@ -0,0 +1,91 @@
+// ==UserScript==
+// @name         Scryfall Tagger Enhancements
+// @namespace    https://github.com/spacemonaut/
+// @version      0.2
+// @description  Enhance the Scryfall tagger. Right now, pagination.
+// @author       spacemonaut
+// @match        *://tagger.scryfall.com/*
+// @grant        none
+// @updateURL    https://raw.githubusercontent.com/spacemonaut/userscripts/master/scryfall-tagger-enhancements/scryfall-tagger-enhancements.user.js
+// ==/UserScript==
+
+(() => {
+
+  'use strict';
+
+  const scriptCss = `
+  .ste.buttons--row {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    margin-top: 20px;
+  }
+  `;
+
+  const overrideCss = `
+  @media (min-width: 850px) {
+    .illustration-detail .card {
+      position: unset;
+    }
+
+    .illustration-detail__card {
+      position: sticky;
+      top: 15px;
+    }
+  }
+  `;
+
+  const card = {};
+
+  let cardPanel = document.getElementsByClassName('illustration-detail__card');
+  if (cardPanel.length > 0) {
+    cardPanel = cardPanel[0];
+    parseUrl();
+    createCss();
+    createNavPanel();
+  }
+
+  function parseUrl() {
+    const particles = location.pathname.split('/');
+    card.setCode = particles[2];
+    // TODO: if set is promo, identify the non-promo set
+    card.number = parseInt(particles[3], 10);
+    card.isBackFace = false;
+    if (particles.length > 4 && particles[4] === 'back') {
+      card.isBackFace = true;
+    }
+  }
+
+  function createCss() {
+    let styles = document.createElement('style');
+    styles.innerHTML = scriptCss;
+    document.head.appendChild(styles);
+
+    let overrideStyles = document.createElement('style');
+    overrideStyles.innerHTML = overrideCss;
+    //document.head.appendChild(overrideStyles);
+  }
+
+  function makeUrlForCard(setCode, number) {
+    return '/card/' + setCode + '/' + number;
+  }
+
+  function createNavPanel() {
+    let navPanel = document.createElement('div');
+    navPanel.className = "ste buttons--row";
+    cardPanel.appendChild(navPanel);
+
+    let prevButton = document.createElement('a');
+    prevButton.innerHTML = "&larr; prev";
+    prevButton.className = "button-primary";
+    prevButton.href = makeUrlForCard(card.setCode, card.number - 1);
+    navPanel.appendChild(prevButton);
+
+    let nextButton = document.createElement('a');
+    nextButton.innerHTML = "next &rarr;";
+    nextButton.className = "button-primary";
+    nextButton.href = makeUrlForCard(card.setCode, card.number + 1);
+    navPanel.appendChild(nextButton);
+  }
+})();
