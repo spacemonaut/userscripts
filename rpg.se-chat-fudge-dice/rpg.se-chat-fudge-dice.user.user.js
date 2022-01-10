87d39c155c98d8c5f29ccc5347649987d47ac034
spacemonaut <4585677+spacemonaut@users.noreply.github.com>
2022-01-10 21:15:17 +0000

    Create rpg.se-chat-fudge-dice.user.user.js

---
+++ rpg.se-chat-fudge-dice/rpg.se-chat-fudge-dice.user.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name        RPG.SE Chat Fudge Dice
+// @namespace   https://github.com/spacemonaut/
+// @description Convert RPG.SE chat d6 to Fudge dice (dF)
+// @grant       none
+// @include     https://chat.stackexchange.com/rooms/*
+// @include     https://chat.stackexchange.com/transcript/*
+// @version     1.6.0
+// @downloadURL https://github.com/spacemonaut/rpgse-chat-fudge-dice/raw/main/src/index.user.js
+// ==/UserScript==
+
+// This is a placeholder to forward auto-update to the new version of the script.
+// This is because 1.5.0 had a broken auto-update to point to this URL.
+
+console.warn(`You're using an outdated version of Fudge dice. Please download the new version: https://github.com/spacemonaut/rpgse-chat-fudge-dice/raw/main/src/index.user.js`);
